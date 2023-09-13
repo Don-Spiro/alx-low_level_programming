ead6feb6c9d9378a31957eac8baf1baf1aa8f0ff
don-spiro <nddugwuoke@gmail.com>
2023-09-13 10:07:35 -0700

    Hello World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int Main (void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
