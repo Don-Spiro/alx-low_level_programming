f44b58be25509d9b8a5cd40f82b945afacc7aced
don-spiro <nddugwuoke@gmail.com>
2023-09-25 13:20:03 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _memset - function that fills the first n bytes of the memory area 
+ * _memset - function that fills the first n bytes of the memory area
  * pointed to by s with the constant byte b
  *@s: first value
  *@b: second value
