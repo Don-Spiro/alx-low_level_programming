60b7507de39c783de6266f7e180c0297f5d9a40e
don-spiro <nddugwuoke@gmail.com>
2023-09-30 09:33:38 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * is_palindrome - function that checks if or not a string is palindrome
+ * @s: string to return
+ *
+ * Return: 1 if a string is a palindrome and 0 if not.
+ */
+int is_palindrome(char *s)
+{
+	int s = is_palindrome;
+
+	if (*S)
+	{
+		return ('\0');
+	}
+	else
+	{
+		return (0);
+	}
+}
